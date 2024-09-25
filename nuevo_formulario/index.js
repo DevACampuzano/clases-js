f96f087ba66d6c13415f22ad9903c555656d6958
1234 <a1234@MacBook-Pro-de-1234.local>
2024-09-24 20:18:34 -0500

    update formulario

--- nuevo_formulario/index.js
+++ nuevo_formulario/index.js
@@ -13,7 +13,59 @@ form.addEventListener("submit", function (event) {
     output[key] = value;
   }
 
-  if (output.pregunta1 == "alto") {
-    log.innerHTML = "Pregunta 1 es correcta";
+  // y en javascript es &&
+  // o en javascript es ||
+
+  if (output.name.trim() === "") {
+    Swal.fire({
+      title: "Complete el formulario",
+      text: "Debe ingresar su nombre en el formulario",
+      icon: "error",
+      showConfirmButton: false,
+      timer: 3000,
+    });
+    return;
+  }
+
+  if (output.mail.trim() === "") {
+    Swal.fire({
+      title: "Complete el formulario",
+      text: "Debe ingresar su correo electronico en el formulario",
+      icon: "error",
+      showConfirmButton: false,
+      timer: 3000,
+    });
+    return;
+  }
+  // pregunta 1 - alto
+  // pregunta 2 - flexible
+
+  // si y sino
+  if (output.pregunta1 !== "alto" || output.pregunta2 !== "flexible") {
+    Swal.fire({
+      title: "No pasaste el examen",
+      icon: "error",
+    });
+  } else {
+    Swal.fire({
+      title: "Pasaste el examen",
+      icon: "success",
+    });
+    const nombre = document.querySelector("#name");
+    const mail = document.querySelector("#mail");
+    const listRest1 = document.querySelectorAll("#res1");
+    const listRest2 = document.querySelectorAll("#res2");
+    const rest3 = document.querySelector("#res3");
+    nombre.value = "";
+    mail.value = "";
+    rest3.checked = false;
+
+    listRest1.forEach((element) => {
+      element.checked = false;
+    });
+
+    listRest2.forEach((element) => {
+      element.checked = false;
+    });
   }
 });
